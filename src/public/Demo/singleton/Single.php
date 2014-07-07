47e535234723d2e11815fbb38b0f1ff23f86653f
Administrateur <Administrateur@ADMINISTRATEUR1.salle2.ip>
2014-07-07 17:16:53 +0200

    Création Singleton

---
+++ src/public/Demo/singleton/Single.php
@@ -0,0 +1,44 @@
+<?php
+
+class Single
+{
+	/**
+	 * Variable de classe qui va contenir notre objet
+	 * @var self
+	 */
+	private static $instances = array();
+	
+	
+	/**
+	 * Singleton = contructeur privé
+	 */
+	final private function __construct()
+	{
+		//Do something here
+	}
+	
+	/**
+	 * Sert à instancier l'objet s'il ne l'est pas et à le renvoyer dans tous les cas
+	 * @return Single $instance
+	 */
+	public static function getInstance()
+	{
+		$class = get_called_class();
+		
+		if (! isset(self::$instances[$class]) ) {
+			self::$instances[$class] = new $class();
+		}
+		
+		return self::$instances[$class];
+	}
+
+	/**
+	 * Méthode privée pour qu'elle ne puisse pas être utilisée
+	 */
+	final public function __clone(){
+		trigger_error('Clonage interdit', E_USER_ERROR);
+	}
+	
+	
+	
+}
---
+++ src/public/Demo/singleton/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once 'Single.php';
+
+$single1 = Single::getInstance();
+var_dump($single1);
+
